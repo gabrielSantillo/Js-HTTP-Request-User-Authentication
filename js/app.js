264e3c71d780873f71bb5115e82b7c3701d3ca41
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-30 12:57:57 -0600

    Commenting my code

--- js/app.js
+++ js/app.js
@@ -1,30 +1,56 @@
+/* when trying to post the email and password is successful, this function is called */
 function post_success(response) {
-    Cookies.set(`token`, `${response[`data`][`token`]}`);
-    location.href = `/home.html`;
-    }
-    
-    function post_failure(error) {
-        login_button.insertAdjacentHTML(`afterend`, `<h1>Sorry, the login was invalid.</h1>`)
-    }
-    
-    function loginFunction(details) {
-        /* use eve.holt@reqres.in */
-        let user_email_input = document.getElementById(`email`);
-        let user_email_value = user_email_input[`value`];
-    
-        /* use cityslicka */
-        let user_password_input = document.getElementById(`password`);
-        let user_password_value = user_password_input[`value`];
-    
-        axios.request({
-            url: `https://reqres.in/api/login`,
-            method: `POST`,
-            data: {
-                email: user_email_value,
-                password: user_password_value
-            }
-        }).then(post_success).catch(post_failure);
-    }
-    
-    let login_button = document.getElementById(`login_button`);
-    login_button.addEventListener(`click`, loginFunction)
+  /* set the key token with token sent from the API*/
+  Cookies.set(`token`, `${response[`data`][`token`]}`);
+  /* the user goes the home page */
+  location.href = `/home.html`;
+}
+
+/* when trying to post the email and password fails, this function is called */
+function post_failure(error) {
+  /* insert a message onto the page asking for the user to try again */
+  login_button.insertAdjacentHTML(
+    `afterend`,
+    `<h1>Sorry, the login was invalid.</h1>`
+  );
+}
+
+/* This function is called after the user typed the email and password and click in the button log in */
+function loginFunction(details) {
+  /* getting the email input by its id from the page
+         use eve.holt@reqres.in when trying to log in*/
+  let user_email_input = document.getElementById(`email`);
+  /* gets the value, that means what the user has typed */
+  let user_email_value = user_email_input[`value`];
+
+  /* getting the password input by its id from the page
+        any password will work */
+  let user_password_input = document.getElementById(`password`);
+  /* gets the value, that means what the user has typed */
+  let user_password_value = user_password_input[`value`];
+
+  /* making an axios request */
+  axios
+    .request({
+      /* the API which the request is being made */
+      url: `https://reqres.in/api/login`,
+      /* posting the valuesm such as email and password */
+      method: `POST`,
+      /* paramater data which the post will be made */
+      data: {
+        /* user email */
+        email: user_email_value,
+        /* user password */
+        password: user_password_value,
+      },
+    })
+    /* if succeeds, call the function post_success */
+    .then(post_success)
+    /* if fails, call the function post_failure */
+    .catch(post_failure);
+}
+
+/* getting the login button by its id */
+let login_button = document.getElementById(`login_button`);
+/* when the button is clicked, call the function loginFunction */
+login_button.addEventListener(`click`, loginFunction);
--- js/home.js
+++ js/home.js
@@ -1,28 +1,41 @@
-let token = Cookies.get(`token`);
-
-if (token !== undefined) {
+/* verify if the cookies is not undefined, that means, if contains value in it */
+if (Cookies.get(`token`) !== undefined) {
+    /* if this is true, make an axios request to this API */
   axios
+  /* API being requested */
     .request({
       url: `https://reqres.in/api/unknown`,
     })
+    /* if succeeds, call the function post_success */
     .then(post_success)
+    /* if fails, call the function post_failure */
     .catch(post_failure);
-} else {
-  article.insertAdjacentHTML(
+} 
+/* if its false, insert onto the page a message and button */
+else {
+    let article = document.querySelector(`article`);
+    article.insertAdjacentHTML(
     `beforeend`,
     `
       <h1>Go back and try a valid email and password</h1>
-      <button id="log_out">Back to log in page</button>
+      <button id="log_in">Back to log in page</button>
       `
   );
 }
 
-function loginFunction(details) {
+/* this function is call if the user is at the page without making any login*/
+function backToLogin(details) {
+    /*leave the user to the login page  */
   location.href = `/index.html`;
+  /* delete the cookie, just in case */
+  Cookies.delete(`token`);
 }
 
+/* if the cookie, contains value, this function is called */
 function post_success(response) {
+    /* for loop that go through each index of the array */
   for (let i = 0; i < response[`data`][`data`].length; i++) {
+    /* insert onto the page, the name of the color, the year that was created and a div that wil contain these color */
     section.insertAdjacentHTML(
       `beforeend`,
       `
@@ -36,8 +49,17 @@ function post_success(response) {
   }
 }
 
-function post_failure(error) {}
+/* if when trying to display the response from the api fails, print this message */
+function post_failure(error) {
+    section.insertAdjacentHTML(
+        /* message that asks for the user to reload the page */
+        `beforeend`, `<h1>Reload the page, please.</h1>`);
+}
 
-let article = document.querySelector(`article`);
-
+/* getting the section from the page with query selector */
 let section = document.querySelector(`section`);
+
+/* getting the button that leave the user back to the login page by its id */
+let back_to_login_page = document.getElementById(`log_in`);
+/* when this button is clicked, call the function backToLogin */
+back_to_login_page.addEventListener(`click`, backToLogin)
